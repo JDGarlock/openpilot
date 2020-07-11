507578d95742caecc1e4dee372fa4b43f96579a3
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-10 20:25:18 -0500

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -52,7 +52,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 18.2
     ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[5.2, 24.], [5.2, 24.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.20], [0.18, 0.22]]     # TODO: tune this was 0.01
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.08, 0.15], [0.10, 0.18]]     # TODO: tune this was 0.01
     ret.lateralTuning.pid.kf =0.00004   # MAX Steer angle to normalize FF
     ret.steerActuatorDelay = 0.1  # Default delay, not measured yet was 0.4
     ret.steerLimitTimer = 0.8
