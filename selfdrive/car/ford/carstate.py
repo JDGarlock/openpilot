16f05bd078ee722c77f22d66602dba22e369573e
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-14 12:27:31 -0500

    Update carstate.py

--- selfdrive/car/ford/carstate.py
+++ selfdrive/car/ford/carstate.py
@@ -34,7 +34,7 @@ def get_can_parser(CP):
     ("Door_FR_Open", "Doors", 0.),
     ("Door_RL_Open", "Doors", 0.),
     ("Door_RR_Open", "Doors", 0.),
-    ("SteeringColumnTorque", "EPAS_INFO", 0.),
+    ("DrvSte_Tq_Actl", "EPAS_INFO", 0.),
   ]
 
   checks = [
@@ -69,10 +69,10 @@ class CarState():
     self.prev_right_blinker_on = self.right_blinker_on
 
     # calc best v_ego estimate, by averaging two opposite corners
-    self.v_wheel_fl = cp.vl["WheelSpeed_CG1"]['WhlRr_W_Meas'] * WHEEL_RADIUS
-    self.v_wheel_fr = cp.vl["WheelSpeed_CG1"]['WhlRl_W_Meas'] * WHEEL_RADIUS
-    self.v_wheel_rl = cp.vl["WheelSpeed_CG1"]['WhlFr_W_Meas'] * WHEEL_RADIUS
-    self.v_wheel_rr = cp.vl["WheelSpeed_CG1"]['WhlFl_W_Meas'] * WHEEL_RADIUS
+    self.v_wheel_fl = cp.vl["WheelSpeed_CG1"]['WhlRr_W_Meas'] * CV.MPH_TO_MS
+    self.v_wheel_fr = cp.vl["WheelSpeed_CG1"]['WhlRl_W_Meas'] * CV.MPH_TO_MS
+    self.v_wheel_rl = cp.vl["WheelSpeed_CG1"]['WhlFr_W_Meas'] * CV.MPH_TO_MS
+    self.v_wheel_rr = cp.vl["WheelSpeed_CG1"]['WhlFl_W_Meas'] * CV.MPH_TO_MS
     v_wheel = mean([self.v_wheel_fl, self.v_wheel_fr, self.v_wheel_rl, self.v_wheel_rr])
 
     # Kalman filter
@@ -90,7 +90,7 @@ class CarState():
     self.pcm_acc_status = cp.vl["Cruise_Status"]['Cruise_State']
     self.main_on = cp.vl["Cruise_Status"]['Cruise_State'] != 0
     self.lkas_state = cp.vl["Lane_Keep_Assist_Status"]['LaActAvail_D_Actl']
-    self.steeringTorque = cp.vl["EPAS_INFO"]['SteeringColumnTorque']
+    self.steeringTorque = cp.vl["EPAS_INFO"]['DrvSte_Tq_Actl']
     self.steer_override = not cp.vl["Lane_Keep_Assist_Status"]['LaHandsOff_B_Actl']
     self.steer_error = cp.vl["Lane_Keep_Assist_Status"]['LaActDeny_B_Actl']
     print ("lkas_state:", self.lkas_state, "steer_override:", self.steer_override, "steer_error:", self.steer_error)
