73a64d666c60a9eec9fb6c9660657b8ebaa874d2
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-14 21:13:27 -0500

    Update carstate.py

--- selfdrive/car/ford/carstate.py
+++ selfdrive/car/ford/carstate.py
@@ -89,7 +89,7 @@ class CarState():
     self.pcm_acc_status = cp.vl["Cruise_Status"]['Cruise_State']
     self.main_on = cp.vl["Cruise_Status"]['Cruise_State'] != 0
     self.lkas_state = cp.vl["Lane_Keep_Assist_Status"]['LaActAvail_D_Actl'
-    self.steer_torque_driver = cp.vl["EPAS_INFO"]['DrvSte_Tq_Actl']
+    self.steeringTorque = cp.vl["EPAS_INFO"]['DrvSte_Tq_Actl']
     self.steer_override = not cp.vl["Lane_Keep_Assist_Status"]['LaHandsOff_B_Actl']
     self.steer_error = cp.vl["Lane_Keep_Assist_Status"]['LaActDeny_B_Actl']
     print ("lkas_state:", self.lkas_state, "steer_override:", self.steer_override, "steer_error:", self.steer_error)
