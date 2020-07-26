bfcb68535b01cfac2044624a7689d1ce50d56428
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-07-26 15:01:35 -0500

    Update fordcan.py

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -1,36 +1,27 @@
 from common.numpy_fast import clip
-from selfdrive.car.ford.values import MAX_ANGLE
+#from selfdrive.car.ford.values import MAX_ANGLE
 
 
-def create_steer_command(packer, angle_cmd, enabled, lkas_state, angle_steers, curvature, lkas_action):
-  """Creates a CAN message for the Ford Steer Command."""
+#def create_steer_command(packer, angle_cmd, enabled, lkas_state, angle_steers, curvature, lkas_action):
+  #"""Creates a CAN message for the Ford Steer Command."""
 
   #if enabled and lkas available:
-  if enabled and lkas_state in [2,3]: #and (frame % 500) >= 3:
-    action = lkas_action
-  else:
-    action = 0xf
+  #if enabled and lkas_state in [2,3]: #and (frame % 500) >= 3:
+  #  action = lkas_action
+  #else:
+  #  action = 0xf
     
-    angle_cmd = angle_steers/MAX_ANGLE
+  #  angle_cmd = angle_steers/MAX_ANGLE
 
-  angle_cmd = clip(angle_cmd * MAX_ANGLE, - MAX_ANGLE, MAX_ANGLE)
+  #angle_cmd = clip(angle_cmd * MAX_ANGLE, - MAX_ANGLE, MAX_ANGLE)
 
-  values = {
-    "Lkas_Action": action,
-    "Lkas_Alert": 0xe,             # no alerts
-    "Lane_Curvature": clip(curvature, -0.03, 0.03),   # is it just for debug?
-    "Steer_Angle_Req": angle_cmd
-  }
-  return packer.make_can_msg("Lane_Keep_Assist_Control", 0, values)
-
-#def create_lkas_status(packer, enabled, lkasState, steer_pressed, steer_error):
-#  """Creates a CAN message for the Ford LKAS State"""
-#  values = {
-#    "LaActAvail_D_Actl": lkasState,
-#    "LaHandsOff_B_Actl": steer_pressed,
-#    "LaActDeny_B_Actl": steer_error,
-#  }
-#  return packer.make_can_msg("Lane_Keep_Assist_Status", 0, values)
+  #values = {
+  #  "Lkas_Action": action,
+  #  "Lkas_Alert": 0xe,             # no alerts
+  #  "Lane_Curvature": clip(curvature, -0.03, 0.03),   # is it just for debug?
+  #  "Steer_Angle_Req": angle_cmd
+  #}
+  #return packer.make_can_msg("Lane_Keep_Assist_Control", 0, values)
 
 def create_lkas_ui(packer, main_on, enabled, steer_alert, defog, ahbc, ahbcramping, config, noipma, stats):
   """Creates a CAN message for the Ford Steer Ui."""
