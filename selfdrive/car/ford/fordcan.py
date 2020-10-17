e3ae994ba642de0792955d1c28d3132f0a41e693
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-10-17 14:34:25 -0500

    add more can messages

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -23,22 +23,40 @@ def create_steer_command(packer, angle_cmd, enabled, angle_steers, lkas_action, 
   }
   return packer.make_can_msg("ParkAid_Data", 2, values)
 
-def create_speed_command(packer, speed):
+def create_speed_command(packer, speed, trlraid, actlnocs, actlnocnt, actlqf, gear):
   """Creates a CAN message for the Ford Speed Command."""
   values = {
+    "VehVTrlrAid_B_Avail": trlraid
+    "VehVActlEng_No_Cs": actlnocs
+    "VehVActlEng_No_Cnt": actlnocnt
+    "VehVActlEng_D_Qf": actlqf
+    "GearRvrse_D_Actl": gear
     "Veh_V_ActlEng": speed
   }
   return packer.make_can_msg("EngVehicleSpThrottle2", 2, values)
-#def create_lkas_status(packer, enabled, lkasState, steer_pressed, steer_error):
-#  """Creates a CAN message for the Ford LKAS State"""
-#  values = {
-#    "LaActAvail_D_Actl": lkasState,
-#    "LaHandsOff_B_Actl": steer_pressed,
-#    "LaActDeny_B_Actl": steer_error,
-#  }
-#  return packer.make_can_msg("Lane_Keep_Assist_Status", 0, values)
 
-def create_lkas_ui(packer, main_on, enabled, steer_alert, defog, ahbc, ahbcramping, config, noipma, stats):
+def create_speed_command2(packer, speed2, longcomp, latcomp, yawcomp):
+  """Creates a CAN message for the Ford Speed Command."""
+  values = {
+    "VehOverGnd_V_Est": speed2
+    "VehLongComp_A_Actl": longcomp
+    "VehLatComp_A_Actl": latcomp
+    "VehYawComp_W_Actl": yawcomp
+  }
+  return packer.make_can_msg("BrakeSnData_3", 2, values)
+
+def create_speed_command3(packer, speed3, lsmcdecel, actlbrknocs, actlbrknocnt, actlbrkqf):
+  """Creates a CAN message for the Ford Speed Command."""
+  values = {
+    "Veh_V_ActlBrk": speed3
+    "LsmcBrkDecel_D_Stat": lsmcdecel
+    "VehVActlBrk_No_Cs": actlbrknocs
+    "VehVActlBrk_No_Cnt": actlbrknocnt
+    "VehVActlBrk_D_Qf": actlbrkqf
+  }
+  return packer.make_can_msg("BrakeSysFeature", 2, values)
+
+def create_lkas_ui(packer, main_on, enabled, steer_alert, defog, ahbc, ahbcramping, config, noipma, stats, persipma, dasdsply):
   """Creates a CAN message for the Ford Steer Ui."""
   if enabled:
     lines = 0x6
@@ -46,8 +64,8 @@ def create_lkas_ui(packer, main_on, enabled, steer_alert, defog, ahbc, ahbcrampi
     lines = 0xc
 
   values = {
-    "PersIndexIpma_D_Actl": 0x80,
-    "DasStats_D_Dsply": 0x45,
+    "PersIndexIpma_D_Actl": persipma,
+    "DasStats_D_Dsply": dasdsply,
     "Set_Me_X30": 0x30,
     "Lines_Hud": lines,
     "Hands_Warning_W_Chime": steer_alert,
