48dcf7d14eb1fb2399b266672cab949905437451
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-10-17 14:36:22 -0500

    it helps to finish statements

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -26,33 +26,33 @@ def create_steer_command(packer, angle_cmd, enabled, angle_steers, lkas_action, 
 def create_speed_command(packer, speed, trlraid, actlnocs, actlnocnt, actlqf, gear):
   """Creates a CAN message for the Ford Speed Command."""
   values = {
-    "VehVTrlrAid_B_Avail": trlraid
-    "VehVActlEng_No_Cs": actlnocs
-    "VehVActlEng_No_Cnt": actlnocnt
-    "VehVActlEng_D_Qf": actlqf
-    "GearRvrse_D_Actl": gear
-    "Veh_V_ActlEng": speed
+    "VehVTrlrAid_B_Avail": trlraid,
+    "VehVActlEng_No_Cs": actlnocs,
+    "VehVActlEng_No_Cnt": actlnocnt,
+    "VehVActlEng_D_Qf": actlqf,
+    "GearRvrse_D_Actl": gear,
+    "Veh_V_ActlEng": speed,
   }
   return packer.make_can_msg("EngVehicleSpThrottle2", 2, values)
 
 def create_speed_command2(packer, speed2, longcomp, latcomp, yawcomp):
   """Creates a CAN message for the Ford Speed Command."""
   values = {
-    "VehOverGnd_V_Est": speed2
-    "VehLongComp_A_Actl": longcomp
-    "VehLatComp_A_Actl": latcomp
-    "VehYawComp_W_Actl": yawcomp
+    "VehOverGnd_V_Est": speed2,
+    "VehLongComp_A_Actl": longcomp,
+    "VehLatComp_A_Actl": latcomp,
+    "VehYawComp_W_Actl": yawcomp,
   }
   return packer.make_can_msg("BrakeSnData_3", 2, values)
 
 def create_speed_command3(packer, speed3, lsmcdecel, actlbrknocs, actlbrknocnt, actlbrkqf):
   """Creates a CAN message for the Ford Speed Command."""
   values = {
-    "Veh_V_ActlBrk": speed3
-    "LsmcBrkDecel_D_Stat": lsmcdecel
-    "VehVActlBrk_No_Cs": actlbrknocs
-    "VehVActlBrk_No_Cnt": actlbrknocnt
-    "VehVActlBrk_D_Qf": actlbrkqf
+    "Veh_V_ActlBrk": speed3,
+    "LsmcBrkDecel_D_Stat": lsmcdecel,
+    "VehVActlBrk_No_Cs": actlbrknocs,
+    "VehVActlBrk_No_Cnt": actlbrknocnt,
+    "VehVActlBrk_D_Qf": actlbrkqf,
   }
   return packer.make_can_msg("BrakeSysFeature", 2, values)
 
