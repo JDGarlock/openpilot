d144306e6cb376e0f7229fbe1380657e154fff7e
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-10-19 22:13:09 -0500

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -87,7 +87,7 @@ class CarInterface(CarInterfaceBase):
     # events
     events = self.create_common_events(ret)
       
-    if self.CC.enabled:
+    if self.CC.enabled_last:
       if self.CS.sappHandshake != 2 and self.CC.sappConfig_last != 16:
         events.add(car.CarEvent.EventName.pscmHandshaking)
       if self.CS.sappHandshake == 2 and self.CC.sappAction >= 3 and self.CC.sappConfig_last == 224:
